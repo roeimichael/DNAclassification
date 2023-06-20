eed4d98799f5fdf1e138b4aefe787fce5d1847b1
roeimichael <roeym111@gmail.com>
2023-06-20 20:29:34 +0300

    added model as ssperate class and fixed modelTraining

--- modelTraining.py
+++ modelTraining.py
@@ -2,14 +2,18 @@ import os
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv1D, Flatten, Dropout
-from tensorflow.keras.metrics import CategoricalAccuracy
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.utils import to_categorical
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+from keras.losses import CategoricalCrossentropy
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import warnings
+from keras.optimizers import Adam
+from keras.metrics import CategoricalAccuracy, Precision, Recall
+from sklearn.metrics import classification_report
+from keras.utils import plot_model
+from CNN1D import CNN1D
+
+warnings.filterwarnings("ignore")
 
 
 def check_gpu_memory():
@@ -25,7 +29,6 @@ def check_gpu_memory():
 def load_data():
     # Load the list of encoded sequence file names
     encoded_files = os.listdir("./data/encoded_sequences/")
-    # Load labels
     reduced_data = pd.read_csv("./data/dataset.csv")
     label_encoder = LabelEncoder()
     labels = label_encoder.fit_transform(reduced_data["lineage"])
@@ -49,82 +52,88 @@ def split_data(encoded_files, labels):
 
 def numpy_load(file, sequence_length):
     sequence = np.load(file.decode('utf-8')).astype(np.float32)
-    # pad sequence if its length is less than the desired sequence_length
+    print(f"Original sequence shape: {sequence.shape}")
     if len(sequence) < sequence_length:
         sequence = np.pad(sequence, pad_width=(0, sequence_length - len(sequence)), mode='constant')
-    sequence = sequence.reshape(-1, 1)
+    print(f"Padded sequence shape: {sequence.shape}")
+    sequence = sequence.reshape(-1, 1)  # Note that we reshaped to (-1, 1) here
+    sequence = np.expand_dims(sequence, axis=0)  # Add another dimension to get a 2D array. The shape becomes (1, sequence_length)
+    print(f"Reshaped sequence shape: {sequence.shape}")
     return sequence
 
 
-def load_and_preprocess_data(file, label, sequence_length):
-    path = tf.strings.join(["./data/encoded_sequences/", file])
-    encoded_sequence = tf.numpy_function(numpy_load, [path, sequence_length], tf.float32)
-    encoded_sequence.set_shape([None, 1])  # Set the shape manually as tf.numpy_function does not set it
-    return encoded_sequence, label
-
-
 def create_datasets(files_train, y_train, files_test, y_test, sequence_length):
-    # Create a tf.data.Dataset object
-    train_dataset = tf.data.Dataset.from_tensor_slices((files_train, y_train))
-    test_dataset = tf.data.Dataset.from_tensor_slices((files_test, y_test))
+    def load_and_preprocess_data(file, label, sequence_length):
+        sequence = np.load(file).astype(np.float32)
+        # print(f"Original sequence shape: {sequence.shape}")
+        if len(sequence) < sequence_length:
+            sequence = np.pad(sequence, pad_width=(0, sequence_length - len(sequence)), mode='constant')
+        # print(f"Padded sequence shape: {sequence.shape}")
+        sequence = sequence.reshape(-1, 1)  # Note that we reshaped to (-1, 1) here
+        # print(f"Reshaped sequence shape: {sequence.shape}")
+        sequence = tf.convert_to_tensor(sequence)
+        label = tf.convert_to_tensor(label)
+        return sequence, label
 
-    # Apply the load_and_preprocess_data function
-    train_dataset = train_dataset.map(lambda x, y: load_and_preprocess_data(x, y, sequence_length))
-    test_dataset = test_dataset.map(lambda x, y: load_and_preprocess_data(x, y, sequence_length))
+    train_sequences, train_labels, test_sequences, test_labels = [], [], [], []
+    for file, label in zip(files_train, y_train):
+        sequence, label = load_and_preprocess_data("./data/encoded_sequences/" + file, label, sequence_length)
+        train_sequences.append(sequence)
+        train_labels.append(label)
 
-    # Shuffle and batch the datasets
-    train_dataset = train_dataset.shuffle(buffer_size=1000).batch(32)
-    test_dataset = test_dataset.shuffle(buffer_size=1000).batch(32)
+    for file, label in zip(files_test, y_test):
+        sequence, label = load_and_preprocess_data("./data/encoded_sequences/" + file, label, sequence_length)
+        test_sequences.append(sequence)
+        test_labels.append(label)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_sequences, train_labels)).shuffle(buffer_size=1000).batch(16)
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_sequences, test_labels)).shuffle(buffer_size=1000).batch(16)
 
     return train_dataset, test_dataset
 
 
-def create_model(sequence_length, num_classes):
-    # Define the model
-    model = Sequential()
-    model.add(Conv1D(64, 5, activation='relu', input_shape=(sequence_length, 1)))  # Convolutional layer
-    model.add(Dropout(0.5))  # Dropout layer to reduce overfitting
-    model.add(Flatten())  # Flatten layer
-    model.add(Dense(128, activation='relu'))  # Dense layer with 128 neurons
-    model.add(Dense(num_classes, activation='softmax'))  # Output layer
-    model.summary()
-    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=[CategoricalAccuracy()])
-
+def create_model(sequence_length, n_classes):
+    model = CNN1D(sequence_length, dropout_rate=0.5,n_classes=n_classes)
+    model.compile(
+        loss=CategoricalCrossentropy(),
+        optimizer=Adam(),
+        metrics=[CategoricalAccuracy(), Precision(), Recall()]
+    )
     return model
 
 
-def evaluate_model(y_test, y_pred):
-    # Convert one-hot encoded labels back to class labels
-    y_test = np.argmax(y_test, axis=1)
+def evaluate_model(y_true, y_pred):
+    # Convert prediction probabilities to class labels
     y_pred = np.argmax(y_pred, axis=1)
+    y_true = np.argmax(y_true, axis=1)
 
-    # Calculate metrics
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='macro')
-    recall = recall_score(y_test, y_pred, average='macro')
-    f1 = f1_score(y_test, y_pred, average='macro')
+    # Compute metrics
+    accuracy = np.mean(y_true == y_pred)
+    report = classification_report(y_true, y_pred, output_dict=True)
+    precision = report['macro avg']['precision']
+    recall = report['macro avg']['recall']
+    f1 = report['macro avg']['f1-score']
 
     return accuracy, precision, recall, f1
 
 
 def save_results(model, accuracy, precision, recall, f1):
-    # Store results
-    results = pd.DataFrame({
-        'model_name': ['CNN'],
-        'parameters': [str(model.get_config())],
-        'accuracy': [accuracy],
-        'precision': [precision],
-        'recall': [recall],
-        'F1_score': [f1]
-    })
-    results.to_csv("model_evaluation.csv", index=False)
+    model.save('model.h5')
+    print(f'Saved model to disk.')
+
+    print('\nClassification Report:\n')
+    print(f'Accuracy: {accuracy}')
+    print(f'Precision: {precision}')
+    print(f'Recall: {recall}')
+    print(f'F1 Score: {f1}')
+
+    # Optionally, save the model architecture as an image
+    plot_model(model, to_file='model.png', show_shapes=True)
+    print('Saved model architecture to disk.')
 
 
-# Main function to orchestrate all the steps
 def main():
     check_gpu_memory()
-
-    # Load and preprocess data
     encoded_files, labels, label_encoder = load_data()
     files_train, files_test, y_train, y_test = split_data(encoded_files, labels)
 
@@ -138,17 +147,16 @@ def main():
 
     # Create and train model
     model = create_model(sequence_length, len(label_encoder.classes_))
-    model.summary()
     model.fit(train_dataset, validation_data=test_dataset, epochs=10)
 
     # Predict on test data
     y_pred = model.predict(test_dataset)
 
-    # Evaluate model
+    # As the data is in batches, we need to unbatch it before evaluation
+    y_test = np.concatenate([y for x, y in test_dataset], axis=0)
+
     accuracy, precision, recall, f1 = evaluate_model(y_test, y_pred)
     print(f'Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1: {f1}')
-
-    # Save results
     save_results(model, accuracy, precision, recall, f1)
 
 
